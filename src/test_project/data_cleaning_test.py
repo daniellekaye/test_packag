2040a6eca99beb8697379749075720fd9f45693f
daniellekaye <dek44014@uga.edu>
2025-11-04 14:13:29 +0000

    working on dates for data cleaning

--- src/test_project/data_cleaning_test.py
+++ src/test_project/data_cleaning_test.py
@@ -3,6 +3,24 @@ import pandas as pd
 path = r'test_package\data\sp500_stocks.csv'
 df = pd.read_csv(path)
 
+# remove null values
 df.dropna(inplace = True)
 
-print(df.head(20))
+# print(df.tail(20))
+
+#find companies with values for all dates
+startDate = df['Date'].min()
+startDate = pd.to_datetime(startDate)
+#print("start: " + str(startDate))
+
+endDate = df['Date'].max()
+endDate = pd.to_datetime(endDate)
+#print("end: " + str(endDate))
+
+dateRange = endDate - startDate
+print("range: " + str(dateRange))
+
+countOfDays = df.groupby('Symbol')['Date'].nunique()
+#print(countOfDays)
+
+# print(f"Data ranges from {startDate} to {endDate}")
